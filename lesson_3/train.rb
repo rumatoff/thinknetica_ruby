da98e6eb26ad69dd03ff83c48c4b0ab7c179f790
Anton Antonov <rumatoff@gmail.com>
2019-08-01 09:33:42 +0300

    Lesson 3

---
+++ lesson_3/train.rb
@@ -0,0 +1,63 @@
+class Train
+  attr_reader :number, :type
+  attr_accessor :speed, :wagons, :route
+  def initialize(number, type, wagons)
+    @number = number
+    @type = type
+    @wagons = wagons
+    @speed = 0
+  end
+
+  def accelerate(speed)
+    self.speed += speed
+  end
+
+  def stop
+    self.speed = 0
+  end
+
+  def move?
+    true if self.speed.positive?
+  end
+
+  def add_wagon
+    self.wagons += 1 unless move?
+  end
+
+  def remove_wagon
+    if self.wagons.zero? || move?
+      false
+    else
+      self.wagons -= 1
+    end
+  end
+
+  def add_route(route)
+    self.route = route
+    route.stations[0].take_train(self)
+  end
+
+  def current_station
+    route.stations.find { |station| station.trains.include?(self) }
+  end
+
+  def previous_station
+    route.stations[route.stations.index(self.current_station) - 1]
+  end
+
+  def next_station
+    route.stations[route.stations.index(self.current_station) + 1]
+  end
+
+  def forward
+    station = next_station
+    current_station.send_train(self)
+    station.take_train(self)
+  end
+
+  def backward
+    station = previous_station
+    current_station.send_train(self)
+    station.take_train(self)
+  end
+end
